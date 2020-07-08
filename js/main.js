220ce5c9ca0abaf3378ffb34cdd6b96063faf680
blake-hodges <tbhodges@gmail.com>
2020-07-08 17:32:33 -0400

    create basic feed of articles from rss feed

--- js/main.js
+++ js/main.js
@@ -4,15 +4,48 @@ fetch(sivers_rss)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
-        //console.log(data)
-;       let articles = data.querySelectorAll("title");
-        for(let i = 1; i < articles.length; i++) {
-            //console.log(articles[i].innerHTML);
-            let li = document.createElement("li");
-            let node = document.createTextNode(articles[i].innerHTML);
-            li.appendChild(node);
-            let articlesListDiv = document.querySelector(".articles-list");
-            articlesListDiv.appendChild(li);
+        console.log(data);
+;       let entries = data.querySelectorAll("entry");
+
+        for(let i = 0; i < entries.length; i++) {
+
+            let author = "Derek Sivers";
+            let title = entries[i].querySelector("title").innerHTML;
+            let date = entries[i].querySelector("updated").innerHTML;
+            let dateObj = new Date(date);
+            let dateString = dateObj.toDateString();
+            let link = entries[i].querySelector("link");
+            let linkURL = link.getAttribute("href");
+            let content = entries[i].querySelector("content").innerHTML;
+
+            let div = document.createElement("div");
+            let h5 = document.createElement("h5");
+            let dateParagraph = document.createElement("p");
+            let contentParagraph = document.createElement("p");
+            let a = document.createElement("a");
+
+
+
+            h5.innerText = title;
+            dateParagraph.innerText = dateString;
+            contentParagraph.innerText = content;
+            a.setAttribute("href", linkURL);
+            a.innerText = "Read";
+
+            div.appendChild(h5);
+            div.appendChild(dateParagraph);
+            //div.appendChild(contentParagraph);
+            div.appendChild(a);
+
+
+            document.querySelector(".articles").appendChild(div);
+
+
+            // let li = document.createElement("li");
+            // let node = document.createTextNode(articles[i].innerHTML);
+            // li.appendChild(node);
+            // let articlesListDiv = document.querySelector(".articles-list");
+            // articlesListDiv.appendChild(li);
 
         }
         //console.log(articles[1].innerHTML);
